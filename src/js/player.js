acbd60dadcc264c9d519a565c800089f5ec3b510
ImReks <makskorn3200kameleon@gmail.com>
2023-06-14 12:20:53 +0200

    Simply dive logic

--- src/js/player.js
+++ src/js/player.js
@@ -2,36 +2,39 @@ import {Actor, Vector, Input, clamp} from "excalibur";
 import {Resources} from "./resources.js";
 export class player extends Actor
 {
+    //Refernce to engine
     game
+    //ID number of the food object that is currently picked
     inventory
-
     angle
-
     isDiving
-    isRising
-
     divingTimer
 
 
     constructor() {
         super();
+        //se up initiaal values for variables
         this.angle=0;
-        this.divingTimer=0;
+        this.divingTimer=-1;
         this.isDiving=false;
-        this.isRising=false;
     }
     onInitialize(_engine) {
         super.onInitialize(_engine);
+        //save refernce to game engine
         this.game=_engine;
+        //assign sprite to actor. The sprite is flipped because it faced wrong direction, might not need in final version
         let sprite = Resources.Fish.toSprite();
         sprite.flipHorizontal=true;
         this.graphics.use(sprite);
     }
     onPreUpdate(_engine, _delta) {
         super.onPreUpdate(_engine, _delta);
+        //Check for player input
         this.PlayerInput(_delta/1000);
        // console.log(this.angle);
+        //Recalculate player velocity
         this.Move();
+        //player dive
         if(this.isDiving)
         {
             this.Dive(_delta/1000)
@@ -40,6 +43,7 @@ export class player extends Actor
 
     PlayerInput(delta)
     {
+        //Increase or Decrease player rotation angle depending on key pressed
         if(this.game.input.keyboard.isHeld(Input.Keys.A))
         {
             this.angle-=delta*5;
@@ -48,6 +52,8 @@ export class player extends Actor
         {
             this.angle+=delta*5;
         }
+
+        //start player dive if space is pressed and the player was not diving
         if(this.game.input.keyboard.isHeld(Input.Keys.Space) && this.isDiving==false)
         {
             this.isDiving=true;
@@ -55,39 +61,35 @@ export class player extends Actor
     }
     Move()
     {
+        //rotate player
         this.transform.rotation=this.angle;
+        //recalculate forward velocity of the player
         let forward = new Vector(Math.cos(this.angle)*300,Math.sin(this.angle)*300)
         this.vel = forward;
     }
 
     Dive(delta)
     {
-        if(!this.isRising) {
-          //  console.log("scaleDown")
-            this.divingTimer += delta;
-        }
-        else
-        {
-          //  console.log("scaleUp")
-            this.divingTimer -= delta;
-        }
+        this.divingTimer +=delta;
+        console.log(this.divingTimer);
+
         let scale = new Vector(0,0);
-        let clampedTime = Math.min(Math.max(0,this.divingTimer),1)
-       // console.log(clampedTime);
-        scale.x = this.lerp(1,0.5,clampedTime)
+        //takes the absolute value of diving timer and clamps it between 0 and 1
+        let clampedTime = Math.min(Math.max(0,Math.abs(this.divingTimer)),1)
+        //lerp player scale between min and max size depending on clamped diving timer value
+        scale.x = this.lerp(0.5,1,clampedTime)
         scale.y = scale.x;
-        console.log(scale.x);
         this.scale=scale;
+        //end dive
         if(this.divingTimer>1)
         {
-            this.isRising=true;
-        }
-        if(this.divingTimer<0)
-        {
-            this.isRising=false;
+            this.divingTimer=-1;
             this.isDiving=false;
         }
     }
+
+
+    //lerp between two numbers
     lerp(a,b,t)
     {
         let v = a+t*(b-a);
