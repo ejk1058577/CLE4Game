52e0812e0f9ef75e05568dea9cec35326f0bfd6d
ImReks <makskorn3200kameleon@gmail.com>
2023-06-15 10:37:51 +0200

    Accidentaly deleted spawner.js this brings it back

---
+++ src/js/spawner.js
@@ -0,0 +1,39 @@
+import {Actor, Random, Vector} from "excalibur";
+
+export class Spawner extends Actor
+{
+    game;
+    targetAmount;
+    currentAmount;
+    spawnObject;
+    spawnParam;
+
+    rng
+    constructor(targetAmount,SpawnObject,SpawnParam) {
+        super();
+        this.targetAmount=targetAmount;
+        this.currentAmount=0;
+        this.spawnObject=SpawnObject;
+        this.spawnParam=SpawnParam;
+    }
+    onInitialize(_engine) {
+        super.onInitialize(_engine);
+        console.log("spawner active")
+        this.game=_engine;
+        this.rng =new Random()
+    }
+
+    onPreUpdate(_engine, _delta) {
+        super.onPreUpdate(_engine, _delta);
+        if(this.currentAmount<this.targetAmount)
+        {
+            let spawn = new this.spawnObject(this.spawnParam);
+            let xpos = this.rng.integer(0,800);
+            let ypos = this.rng.integer(0,600);
+            spawn.pos = new Vector(xpos,ypos);
+            spawn.spawner = this;
+            this.scene.add(spawn);
+            this.currentAmount++;
+        }
+    }
+}
